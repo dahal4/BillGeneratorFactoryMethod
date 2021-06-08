b99680dfb871722a1a575e9f699d06e60f29fe88
manish <manishdahal42@gmail.com>
2021-06-08 17:44:40 +0545

    ui friendly

--- GenerateBill.java
+++ GenerateBill.java
@@ -17,11 +17,15 @@ public class GenerateBill {
 
     public static void main(String[] args) throws IOException {
         GetMenuFactory itemFactory = new GetMenuFactory();
+        System.out.println("************************************************************************");
         System.out.println("Select the menu items:");
+        System.out.println("************************************************************************");
         System.out.println("1.Momo");
         System.out.println("2.French Fries");
         System.out.println("3.Chow mein");
         System.out.println("4.Exit");
+        System.out.println("************************************************************************");
+        System.out.print("=>");
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int choice = Integer.parseInt(br.readLine());
         String item = null;
@@ -29,20 +33,20 @@ public class GenerateBill {
         switch (choice) {
             case 1: {
                 item = "Momo";
-                System.out.println("Enter the quantity:");
+                System.out.print("Enter the quantity:");
                 quantity = Integer.parseInt(br.readLine());
                 break;
             }
             case 2: {
                 item = "French Fries";
-                System.out.println("Enter the quantity:");
+                System.out.print("Enter the quantity:");
                 quantity = Integer.parseInt(br.readLine());
                 break;
 
             }
             case 3: {
                 item = "Chow mein";
-                System.out.println("Enter the quantity:");
+                System.out.print("Enter the quantity:");
                 quantity = Integer.parseInt(br.readLine());
                 break;
 
@@ -54,10 +58,14 @@ public class GenerateBill {
         if (item != null) {
             Menu p = itemFactory.getItem(item);
 
-            System.out.println("Bill amount for " + item + " of " + quantity + " plates is:");
+            System.out.print("Bill amount for " + item + " of " + quantity + " plates is:");
             p.getRate();
             p.calculatebill(quantity);
-            
+            System.out.println("************************************************************************");
+            System.out.println("");
+            System.out.println("");
+            main(args);
+
         }
 
     }
